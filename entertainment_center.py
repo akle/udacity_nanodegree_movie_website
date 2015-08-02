582d719464e729d8cc0bc7fd827777f286091848
Pep <joseakle@gmail.com>
2015-08-02 12:24:13 -0500

    fix:
    Formatting to conform to pep8.
    
    feature:
    
    Added director name.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,13 +1,23 @@
 import media
 import fresh_tomatoes
 
-#We create a couple of movies
-toy_story = media.Movie("Toy Story", "Toys that behave like people",  "https://www.movieposter.com/posters/archive/main/98/MPW-49351", "www.youtube.com/watch?v=3jC6McxjCnE")
+# We create a couple of movies
+toy_story = media.Movie(
+	"Toy Story",
+	"Toys that behave like people", 
+	"https://www.movieposter.com/posters/archive/main/98/MPW-49351",
+	"www.youtube.com/watch?v=3jC6McxjCnE",
+	"John Lasseter")
 
-avatar = media.Movie("Avatar", "Blue people with guns.", "http://www.impawards.com/2009/posters/avatar_ver5.jpg", "https://www.youtube.com/watch?v=cX0R3mXaod8")
+avatar = media.Movie(
+	"Avatar",
+	"Blue people with guns.",
+	"http://www.impawards.com/2009/posters/avatar_ver5.jpg",
+	"https://www.youtube.com/watch?v=cX0R3mXaod8",
+	"James Cameron")
 
-#Store the movies in an array.
+# Store the movies in an array.
 movies = [toy_story, avatar]
 
-#Pass the array to the page maker.
+# Pass the array to the page maker.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -4,11 +4,12 @@ class Movie():
     """
     Creates a Movie.
     """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, director):
         self.title =  movie_title
         self.storyline =  movie_storyline
         self.trailer_youtube_url = trailer_youtube
         self.poster_image_url = poster_image
+        self.director = director
 
     def show_trailer(self):
         """
